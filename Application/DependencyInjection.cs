c899c336e6b4e50ca4aa04155c6d2c3d2ee6db1c
Gabriel Taliwongso <gabriel.taliwongso@gmail.com>
2023-07-24 08:32:38 +0700

    Add project files.

---
+++ Application/DependencyInjection.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddApplication(this IServiceCollection s)
+    {
+        var assembly = typeof(DependencyInjection).Assembly;
+
+        s.AddMediatR(c =>
+            c.RegisterServicesFromAssembly(assembly));
+
+        s.AddValidatorsFromAssembly(assembly);
+
+        return s;
+    }
+}
